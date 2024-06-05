da9416557f988ca16b78b012fa701d0860e7757b
erosalpe <eros.alpegiani@gmail.com>
2024-06-05 15:10:45 +0200

    classi

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var persona = {
+"use strict";
+let persona = {
     nome: 'luca',
     cognome: 'rossi',
     eta: 23,
@@ -8,26 +9,61 @@ var persona = {
         citta: 'milano',
     },
 };
-var array = [3, 2, 1];
-var tupla = ['gino', 'franco'];
-var union = 123;
+const array = [3, 2, 1];
+const tupla = ['gino', 'franco'];
+let union = 123;
 var Ruolo;
 (function (Ruolo) {
     Ruolo["ADMIN"] = "admin";
     Ruolo["USER"] = "user";
     Ruolo["GUEST"] = "guest";
 })(Ruolo || (Ruolo = {}));
-var ruolo = Ruolo.ADMIN;
+let ruolo = Ruolo.ADMIN;
 function somma(num1, num2) {
     return num1 + num2;
 }
-var prova;
+let prova;
 prova = somma;
-console.log(prova(4, 5, function () {
-    console.log('funzione cb');
+class Cane {
+    constructor(nome, razza) {
+        this.nome = nome;
+        this.razza = razza;
+    }
+    parla() {
+        console.log(`Ciao sono ${this.nome} e sono un ${this.razza}`);
+    }
+    saluta(cane) {
+        console.log(`Ciao ${cane.nome} io sono ${this.nome} e sono un ${this.razza}`);
+    }
+}
+class IlMioCane {
+    constructor(nome, razza, giocoPreferito) {
+        this.nome = nome;
+        this.razza = razza;
+        this.giocoPreferito = giocoPreferito;
+    }
+    static getInstance() {
+        if (IlMioCane.instance) {
+            return this.instance;
+        }
+        this.instance = new IlMioCane('franco', 'carlino', 'pallina');
+        return this.instance;
+    }
+    presenta() {
+        console.log(`Ciao io sono ${this.nome} sono un ${this.razza} e il mio gioco prefetiro è la ${this.giocoPreferito}`);
+    }
+}
+let cane1 = new Cane('luna', 'bassotto');
+let cane2 = new Cane('stella', 'alano');
+console.log(prova(4, 5, () => {
+    console.log('funzione callback chiamata!');
 }));
 console.log(persona);
 console.log(array);
 console.log(tupla);
 console.log(union);
 console.log(ruolo);
+console.log(cane1);
+cane1.parla();
+cane2.saluta(cane1);
+IlMioCane.getInstance().presenta();
--- app.ts
+++ app.ts
@@ -40,11 +40,55 @@ function somma(num1: number, num2: number): number{
 let prova: (x: number, y: number, cb: (result: string) =>void) => number;
 prova = somma;
 
+class Cane{
+
+    constructor(private nome: string, private razza: string){
+    }
+
+    parla(): void{
+        console.log(`Ciao sono ${this.nome} e sono un ${this.razza}`);
+    }
+
+    saluta(cane: Cane): void{
+        console.log(`Ciao ${cane.nome} io sono ${this.nome} e sono un ${this.razza}`)
+    }
+}
+
+class IlMioCane{
+    private static instance: IlMioCane;
+
+    private constructor(private nome: string, private razza: string, private giocoPreferito: string){
+    }
+
+    static getInstance(){
+        if(IlMioCane.instance){
+            return this.instance
+        }
+
+        this.instance = new IlMioCane('franco', 'carlino', 'pallina');
+        return this.instance;
+    }
+
+    presenta(){
+        console.log(`Ciao io sono ${this.nome} sono un ${this.razza} e il mio gioco prefetiro è la ${this.giocoPreferito}`)
+    }
+}
+
+let cane1: Cane = new Cane('luna', 'bassotto');
+
+let cane2: Cane = new Cane('stella', 'alano');
+
 console.log(prova(4,5, ()=>{
-    console.log('funzione cb');
+    console.log('funzione callback chiamata!');
 }));
 console.log(persona);
 console.log(array);
 console.log(tupla);
 console.log(union);
 console.log(ruolo);
+console.log(cane1);
+
+cane1.parla();
+cane2.saluta(cane1);
+
+IlMioCane.getInstance().presenta();
