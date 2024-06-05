8cc844e5ecb82e73de6c9e2741cb2c9e717090e3
erosalpe <eros.alpegiani@gmail.com>
2024-06-05 13:52:48 +0200

    array tuple union enum

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-function somma(a, b) {
-    return a + b;
+var persona = {
+    nome: 'luca',
+    cognome: 'rossi',
+    eta: 23,
+    indirizzo: {
+        via: 'giorgi',
+        civico: 3,
+        citta: 'milano',
+    },
+};
+var array = [3, 2, 1];
+var tupla = ['gino', 'franco'];
+var union = 123;
+var Ruolo;
+(function (Ruolo) {
+    Ruolo["ADMIN"] = "admin";
+    Ruolo["USER"] = "user";
+    Ruolo["GUEST"] = "guest";
+})(Ruolo || (Ruolo = {}));
+var ruolo = Ruolo.ADMIN;
+function somma(num1, num2) {
+    return num1 + num2;
 }
-console.log(somma(2, 4));
+var prova;
+prova = somma;
+console.log(prova(4, 5, function () {
+    console.log('funzione cb');
+}));
+console.log(persona);
+console.log(array);
+console.log(tupla);
+console.log(union);
+console.log(ruolo);
--- app.ts
+++ app.ts
@@ -1,5 +1,50 @@
-function somma(a: number,b:number){
-    return a + b;
+type Persona = {
+    nome: string,
+    cognome: string,
+    eta: number,
+    indirizzo: {
+        via: string,
+        civico: number,
+        citta: string,
+    },
+} 
+let persona: Persona = {
+    nome: 'luca',
+    cognome: 'rossi',
+    eta: 23,
+    indirizzo: {
+        via: 'giorgi',
+        civico: 3,
+        citta: 'milano',
+    },
+};
+
+const array: number[] = [3,2,1];
+
+const tupla: [string, string] = ['gino', 'franco'];
+
+let union: string | number = 123;
+
+enum Ruolo {
+    ADMIN = 'admin',
+    USER = 'user',
+    GUEST = 'guest'
 }
 
-console.log(somma(2,4));
+let ruolo = Ruolo.ADMIN;
+
+function somma(num1: number, num2: number): number{
+    return num1 + num2;
+}
+
+let prova: (x: number, y: number, cb: (result: string) =>void) => number;
+prova = somma;
+
+console.log(prova(4,5, ()=>{
+    console.log('funzione cb');
+}));
+console.log(persona);
+console.log(array);
+console.log(tupla);
+console.log(union);
+console.log(ruolo);
